8f53adba77e61374e2ceabdad0868ba668fbcf6b
Thomas Marsh <thomas@fiftythree.com>
2017-01-08 14:14:15 -0500

    problem 26

---
+++ 026/cycle.py
@@ -0,0 +1,16 @@
+def cycle(n, d):
+    for i in range(1, d):
+        if 1 == 10**i % d:
+            return i
+    return 0
+
+def f(n):
+    assert(n > 2)
+    best = 0, None
+    for i in range(2, n):
+        c = cycle(1, i)
+        if c > best[0]:
+            best = c, i
+    return best[1]
+
+print f(1000)

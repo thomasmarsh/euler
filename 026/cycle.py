1d133d1a7ef70fc7e5ad5ca4d59842d53c48c5f8
Thomas Marsh <thomas@fiftythree.com>
2017-01-08 20:54:06 -0500

    remove unneeded parameter

--- 026/cycle.py
+++ 026/cycle.py
@@ -1,4 +1,4 @@
-def cycle(n, d):
+def cycle(d):
     for i in range(1, d):
         if 1 == 10**i % d:
             return i
@@ -8,7 +8,7 @@ def f(n):
     assert(n > 2)
     best = 0, None
     for i in range(2, n):
-        c = cycle(1, i)
+        c = cycle(i)
         if c > best[0]:
             best = c, i
     return best[1]

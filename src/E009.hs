a97e57b08bf091a46d1d59cb3491217676292096
Thomas Marsh <tmarsh@spotify.com>
2018-07-07 11:31:42 -0400

    reformat

--- src/E009.hs
+++ src/E009.hs
@@ -1,17 +1,18 @@
 module E009 (e9) where
 
-isInt x = x == fromInteger (round x)
-
 triplets :: Int -> [[Int]]
-triplets n = [[a,b,c] | a <- [0..(n-1)],
-                        b <- [a+1..n],
-                        let x = sqrt $ fromIntegral (a*a+b*b),
-                        let c = truncate x,
-                        let a' = fromIntegral a,
-                        let b' = fromIntegral b,
-                        a'+b'+x == fromIntegral n,
-                        x > b']
+triplets n =
+    [ [a,b,c]
+    | a <- [0..n-1]
+    , b <- [a+1..n]
+    , let x = sqrt $ fromIntegral (a*a+b*b)
+    , let c = truncate x
+    , let a' = fromIntegral a
+    , let b' = fromIntegral b
+    , a'+b'+x == fromIntegral n
+    , x > b'
+    ]
 
-f n = product . head $ triplets n
+f = product . head . triplets
 
 e9 = fromIntegral $ f 1000

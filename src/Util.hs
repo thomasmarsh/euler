c0ece7336dfaec89afee6c45e1b8c46311ce0ad7
Thomas Marsh <thomas@fiftythree.com>
2019-03-31 12:05:13 -0400

    format

--- src/Util.hs
+++ src/Util.hs
@@ -6,7 +6,10 @@ module Util
 import Data.Digits (digits, unDigits)
 import Data.Profunctor (Profunctor(..), dimap)
 
-onDigits :: (Profunctor p, Integral n, Integral d) => p [n] [d] -> p n d
+onDigits
+    :: (Profunctor p, Integral n, Integral d)
+    => p [n] [d]
+    -> p n d
 onDigits = dimap (digits 10) (unDigits 10)
 
 --Returns if x is an int to n decimal places

12003fad76405016a3cc1aa1d6c4b4d49c5b8c81
Thomas Marsh <thomas@fiftythree.com>
2019-03-31 12:56:59 -0400

    add type sigs

--- src/E047.hs
+++ src/E047.hs
@@ -3,10 +3,14 @@ module E047 (e47) where
 import Data.Numbers.Primes (primeFactors)
 import Data.List (nub)
 
+ns :: [(Integer, Int)]
 ns = map (\x -> (x, length $ nub $ primeFactors x)) [1..]
 
+isMatch :: [(a, Int)] -> Bool
 isMatch = all (4 ==) . map snd . take 4
 
+f :: [(a, Int)] -> a
 f xs@((n,_):rest) = if isMatch xs then n else f rest
 
-e47 = fromIntegral $ f ns
+e47 :: Integer
+e47 = f ns

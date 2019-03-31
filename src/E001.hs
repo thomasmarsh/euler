e0c59136f4d0ed8a624899e35b424d032b23bcef
Thomas Marsh <thomas@fiftythree.com>
2019-03-30 21:21:18 -0400

    cleanup

--- src/E001.hs
+++ src/E001.hs
@@ -1,10 +1,15 @@
 module E001 (e1) where
 
-is3or5multiple x
-  | x `mod` 3 == 0 = True
-  | x `mod` 5 == 0 = True
-  | otherwise = False
+import Control.Applicative (liftA2)
 
-f n = sum $ filter is3or5multiple [1..n-1]
+multipleOf :: Integral a => a -> a-> Bool
+multipleOf n x = x `mod` n == 0
 
+f :: Integer -> Integer
+f n = sum
+    . filter (liftA2 (||) (multipleOf 3)
+                          (multipleOf 5))
+    $ [1..n-1]
+
+e1 :: Integer
 e1 = f 1000

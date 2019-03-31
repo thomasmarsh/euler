de10545252410557f843c788b58d97398855856b
Thomas Marsh <thomas@fiftythree.com>
2019-03-30 21:22:15 -0400

    add type sigs

--- src/E002.hs
+++ src/E002.hs
@@ -1,7 +1,10 @@
 module E002 (e2) where
 
+fibs :: [Integer]
 fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
 
+f :: Integer -> Integer
 f n = sum $ filter even (takeWhile (< n) fibs)
 
+e2 :: Integer
 e2 = f 4000000
